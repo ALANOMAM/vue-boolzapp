ea0bfebf6464780f99c442ff34c39b1e06ba2504
alan_omam <omamalan@hotmail.it>
2024-02-22 11:57:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const { createApp } = Vue
+
+
+createApp({
+    data() {
+        return {
+           
+            
+        }
+    },
+    
+    methods:{
+
+        
+    },
+   
+
+    
+     /*mounted() {
+        
+
+        axios.get('API LINK').then((variabile) => {
+           
+        });
+    }*/
+}).mount('#app');
